82f2b32ec5282a7f35c472b359de3a0bfbdccbdd
whCloudht <liudan11@chinatelecom.cn>
2023-12-18 16:41:55 +0800

    测试

--- test.py
+++ test.py
@@ -6,4 +6,10 @@ result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE,
 
 # 获取输出
 output = result.stdout
-print("Command Output:\n", output)
+print("Command Output1:\n", output)
+command = """kubectl get service -n kourier-system -o=jsonpath='{.status.url}'"""
+result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+
+# 获取输出
+output = result.stdout
+print("Command Output2:\n", output)
