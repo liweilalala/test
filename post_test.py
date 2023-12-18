cb32716d6fd9ea1619e8cdf07bc5a918f152d487
whCloudht <liudan11@chinatelecom.cn>
2023-12-18 16:58:56 +0800

    测试

---
+++ post_test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@File ：post_test.py
+"""
+import requests
+import json
+
+if __name__ == '__main__':
+    header = {
+        "Host": "test-hello.test1.example.com"
+    }
+    # 发送json
+    post_json = json.dumps({'some': 'data'})
+    r1 = requests.post("http://10.99.192.72", data=post_json, headers=header, cookie=cookie)
+    print("r1返回的内容为-->" + r1.text)
